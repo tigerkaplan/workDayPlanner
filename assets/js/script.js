bdb82e131f54a223d208a0ed940b6ca00aebb03e
tigerkaplan <husniyeerparun@gmail.com>
2023-11-09 22:10:37 +0000

    css folder created / index. html file updated with new css folder

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Display the current day at the top of the calender when a user opens the planner.
+
+// Present time-blocks for standard business hours when the user scrolls down.
+
+// Color-code each time-block based on past, present, and future when the time-block is viewed.
+
+// Allow a user to enter an event when they click a time-block
+
+// Save the event in local storage when the save button is clicked in that time-block.
+
+// Persist events between refreshes of a page
