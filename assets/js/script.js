93f1cb21cb9fabd0bc237a28f399e62aa1035e93
tigerkaplan <husniyeerparun@gmail.com>
2023-11-15 10:48:57 +0000

    screenshot added to imaes folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-// Display the current day at the top of the calender when a user opens the planner.
+
 // Adding the current day to calendar
 var currentDate = dayjs().format('dddd, MMMM D, YYYY');
 $("#currentDay").text(currentDate);
+
 // Present time-blocks for standard business hours when the user scrolls down.
 
 // Color-code each time-block based on past, present, and future when the time-block is viewed.
