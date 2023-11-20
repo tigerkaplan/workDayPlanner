6df33a88f168159d6e1fb7d5d92966f1b3a9650b
tigerkaplan <husniyeerparun@gmail.com>
2023-11-20 11:30:31 +0000

    color code updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,20 @@ $("#currentDay").text(currentDate);
 
 // Present time-blocks for standard business hours when the user scrolls down.
 
-// Color-code each time-block based on past, present, and future when the time-block is viewed.
+// Color-code each timeBlock based on past, present, and future when the timeBlock is viewed.
+var presentTime =dayjs().hour();  // Current hour
+$(".time-block").each(function(){
+  var blockedHour = parseInt($(this).text().split("")[0]);
+  if (blockedHour < presentTime) {
+  $(this).addClass("past");
+  } else if (blockedHour === presentTime) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
+// Allow a user to enter an event when they click a timeBlock
 
-// Allow a user to enter an event when they click a time-block
-
-// Save the event in local storage when the save button is clicked in that time-block.
+// Save the event in local storage when the save button is clicked in that timeBlock.
 
 // Persist events between refreshes of a page
